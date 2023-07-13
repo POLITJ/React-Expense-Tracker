28a9345af4df2b0f6b8bcdcf756e32e699f9d219
Jose Polit <jospolit@gmail.com>
2023-07-13 12:04:35 +0200

    add and hook-up expenseitem.js and expensedate.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Potatoes", amount: 24.67, date: new Date(2021, 5, 28) },
+    { title: "Toilet Paper", amount: 94.67, date: new Date(2021, 3, 15) },
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 6, 28) },
+  ];
+
   return (
     <div className="App">
-      <h1>  Let's get started</h1>
+      <h1> Let's get started</h1>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
