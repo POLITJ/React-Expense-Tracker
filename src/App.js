35835809a1a44c5daff41be9de34b9a4d5203a2c
Jose Polit <jospolit@gmail.com>
2023-07-14 15:07:08 +0200

    add year filter function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  { id: "e2", title: "Potatoes", amount: 24.67, date: new Date(2021, 5, 28) },
+  {
+    id: "e3",
+    title: "Toilet Papers",
+    amount: 94.67,
+    date: new Date(2021, 3, 15),
+  },
+  {
+    id: "e4",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 6, 28),
+  },
+];
+
 function App() {
-  const expenses = [
-    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: "Potatoes", amount: 24.67, date: new Date(2021, 5, 28) },
-    { title: "Toilet Paper", amount: 94.67, date: new Date(2021, 3, 15) },
-    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 6, 28) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
